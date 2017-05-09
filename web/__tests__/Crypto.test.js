ae5d5c5361611c01f3e131c93641a3189f864883
James Lal <james@lightsofapollo.com>
2017-05-09 15:31:29 -0700

    Add general purpose encrypt/decrypt utilities.
     - Built on top of AES-GCM chiper (WebCrypto).

--- web/__tests__/Crypto.test.js
+++ web/__tests__/Crypto.test.js
@@ -1,6 +1,11 @@
+/* @flow */
 import * as Crypto from '../src/Crypto';
 
 describe('Crypto', () => {
+
+  function hex(input) {
+    return new Buffer(input).toString('hex');
+  }
 
   describe('pbkdf2', () => {
     const common = { iterations: 5, salt: 'salt', };
@@ -17,12 +22,22 @@ describe('Crypto', () => {
           Crypto.pbkdf2(pass, opt),
           Crypto.pbkdf2(pass, opt),
         ]);
-        expect(one.toString('hex')).toBe(two.toString('hex'));
-
+        expect(hex(one)).toBe(hex(two));
         const bytes = parseInt(opt.hash.replace('SHA-', ''), 10);
-        expect(one.length).toBe(bytes);
+        expect(one.byteLength).toBe(bytes);
       });
     }
+  });
+
+  describe('encrypt/decrypt', () => {
+    it('should work in/out', async () => {
+      const text = 'foo';
+      const pass = 'password';
+
+      const enc = await Crypto.encrypt(text, pass);
+      const dec = await Crypto.decrypt(enc, pass);
+      expect(dec).toBe(text);
+    });
   });
 
 });
--- web/src/Crypto.js
+++ web/src/Crypto.js
@@ -4,8 +4,19 @@
  * @flow
  */
 
-import { crypto, TextEncoder } from './web';
+import { crypto, TextEncoder, TextDecoder, } from './web';
 const subtle = crypto.subtle;
+
+type PBKDF2Opts = {
+  +salt: string,
+  +iterations: number,
+  +hash: 'SHA-256' | 'SHA-384' | 'SHA-512',
+};
+
+type EncryptOut = {
+  +encrypted: ArrayBuffer,
+  +iv: ArrayBuffer,
+};
 
 const HASH_TO_BIT_LENGTH = {
   'SHA-256': 256 << 3,
@@ -13,46 +24,73 @@ const HASH_TO_BIT_LENGTH = {
   'SHA-512': 512 << 3,
 };
 
-export async function encrypt() {
-
+function encode(input: string): ArrayBuffer {
+  // $FlowFixMe: Bug says constructor cannot be called.
+  const enc = new TextEncoder();
+  return enc.encode(input);
 }
 
-export async function decrypt() {
-
+function decode(input: ArrayBuffer): string {
+  // $FlowFixMe: Bug says constructor cannot be called.
+  const decode = new TextDecoder();
+  return decode.decode(input);
 }
 
-type PBKDF2Opts = {
-  +salt: string,
-  +iterations: number,
-  +hash: 'SHA-256' | 'SHA-384' | 'SHA-512',
-} ;
+async function sha256(input: string) {
+  return crypto.subtle.digest('SHA-256', encode(input));
+}
+
+export async function encrypt(
+  plaintext: string, password: string
+): Promise<EncryptOut> {
+  const passwordHash = await sha256(password);
+  /**
+   * Spec states ideal IV is 12 bytes (96 bits).
+   * @see http://csrc.nist.gov/groups/ST/toolkit/BCM/documents/proposedmodes/gcm/gcm-spec.pdf
+   */
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+  const alg = { name: 'AES-GCM', iv, };
+  const key = await subtle.importKey(
+    'raw', passwordHash, alg, false, ['encrypt', ]
+  );
+  const encryptedText = await subtle.encrypt(alg, key, encode(plaintext));
+  return {
+    iv,
+    encrypted: encryptedText,
+  };
+}
+
+export async function decrypt(
+  {encrypted, iv, }: EncryptOut, password: string
+): Promise<string> {
+  const passwordHash = await sha256(password);
+  const alg = { name: 'AES-GCM', iv, };
+  const key = await subtle.importKey(
+    'raw', passwordHash, alg, false, ['decrypt', ]
+  );
+  const decryptBuffer = await subtle.decrypt(alg, key, encrypted);
+  return decode(decryptBuffer);
+}
 
 export async function pbkdf2(
   password: string, options: PBKDF2Opts
-): Buffer {
+): Promise<ArrayBuffer> {
   const {salt, iterations, hash, } = options;
-
-  const enc = new TextEncoder();
-  const passwordInput = enc.encode(password);
-  const saltInput = enc.encode(salt);
-
   const key = await subtle.importKey(
     'raw',
-    passwordInput,
+    encode(password),
     { name: 'PBKDF2', },
     false,
     ['deriveBits', ],
   );
 
   const bitLength = HASH_TO_BIT_LENGTH[hash];
-  const bits = await subtle.deriveBits({
+  return subtle.deriveBits({
     name: 'PBKDF2',
-    salt: saltInput,
+    salt: encode(salt),
     iterations,
     hash: {
       name: hash,
     },
   }, key, bitLength);
-
-  return Buffer.from(bits);
 }
